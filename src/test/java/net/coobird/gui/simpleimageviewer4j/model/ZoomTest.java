d4ee2171ebc29c69969771767fff1845971fbfc7
Chris Kroells <coobird@coobird.net>
2023-07-17 02:07:20 +0900

    Add test cases for zooming.

--- src/test/java/net/coobird/gui/simpleimageviewer4j/model/ZoomTest.java
+++ src/test/java/net/coobird/gui/simpleimageviewer4j/model/ZoomTest.java
@@ -45,4 +45,66 @@ public class ZoomTest {
         new Zoom(new double[] { 0.5, 1.0, 2.0 }, -1);
     }
 
+    @Test
+    public void zoomInWithinBoundary() {
+        // given
+        Zoom zoom = new Zoom(new double[] { 0.5, 1.0, 2.0 }, 1);
+
+        // when
+        zoom.zoomIn();
+
+        // then
+        assertEquals(2.0, zoom.getMagnification(), 0.001);
+    }
+
+    @Test
+    public void zoomOutWithinBoundary() {
+        // given
+        Zoom zoom = new Zoom(new double[] { 0.5, 1.0, 2.0 }, 1);
+
+        // when
+        zoom.zoomOut();
+
+        // then
+        assertEquals(0.5, zoom.getMagnification(), 0.001);
+    }
+
+    @Test
+    public void zoomInPastBoundary() {
+        // given
+        Zoom zoom = new Zoom(new double[] { 0.5, 1.0, 2.0 }, 1);
+
+        // when
+        zoom.zoomIn();
+        zoom.zoomIn();
+
+        // then
+        assertEquals(2.0, zoom.getMagnification(), 0.001);
+    }
+
+    @Test
+    public void zoomOutPastBoundary() {
+        // given
+        Zoom zoom = new Zoom(new double[] { 0.5, 1.0, 2.0 }, 1);
+
+        // when
+        zoom.zoomOut();
+        zoom.zoomOut();
+
+        // then
+        assertEquals(0.5, zoom.getMagnification(), 0.001);
+    }
+
+    @Test
+    public void zoomInAndOutSamePlace() {
+        // given
+        Zoom zoom = new Zoom(new double[] { 0.5, 1.0, 2.0 }, 1);
+
+        // when
+        zoom.zoomIn();
+        zoom.zoomOut();
+
+        // then
+        assertEquals(1.0, zoom.getMagnification(), 0.001);
+    }
 }
